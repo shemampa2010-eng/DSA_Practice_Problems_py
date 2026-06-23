1222df4f2f331591153f1658d22e7764fb52e11b
Paulin <shemampa2010@gmail.com>
2026-06-23 12:16:54 +0200

    a program that takes a string and returns it in reverse order

--- dsa1.py
+++ dsa1.py
@@ -1,13 +1,14 @@
+def reverse(world):
+    mot_a_lenvers = world[::-1]
+    return mot_a_lenvers
 while True :
-    world = input("Un palindrome svp: ")
-    mot = world.lower ()
-    mot_a_lenvers = mot[::-1]
-    if mot == mot_a_lenvers :
-       
+    fish = input("Passe un mot pour voir si c'est un palindrome: ")
+    mot_a_lenvers = reverse(fish)
+    if fish == mot_a_lenvers :
         print ("Lu a l'envers: ", mot_a_lenvers)
         print ("Votre mot est bel et bien un palindrome")
         break
-    elif mot == "break":
+    elif fish == "break":
         break
     else :
         print ("Lu a l'envers: ", mot_a_lenvers)
---
+++ dsa2.py
@@ -0,0 +1,7 @@
+def reverse():
+    world = input("Votre mot sera renverser: ")
+    mot = world.lower ()
+    mot_a_lenvers = mot[::-1]
+    print (mot_a_lenvers)
+
+reverse()
