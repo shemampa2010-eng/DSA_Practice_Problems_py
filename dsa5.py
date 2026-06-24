b71d46d47842e15f0336e07224e36cae6e4b32e0
Paulin <shemampa2010@gmail.com>
2026-06-24 17:03:08 +0200

    count how many times each character appears

--- dsa5.py
+++ dsa5.py
@@ -1,6 +1,15 @@
-word = input("Enter a word: ")
+def does(letters, target):
+    for i in range(0, len(letters)) :
+        if letters[i]["L"] == target :
+            return True , i 
+    return False , -1
 seen = []
+
+word = input("Enter a word: ")
 for letter in word:
-    if letter not in seen:
-        print(letter, ":", word.count(letter))
-        seen.append(letter)
+    does_it_exists = does(seen, letter)
+    if not does_it_exists[0]:
+        seen.append({"L": letter, "f":1})
+    else:
+        seen[does_it_exists[1]]["f"] += 1 
+print(seen)
