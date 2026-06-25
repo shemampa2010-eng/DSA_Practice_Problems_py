b57eb36f8987c186af3c63db077773f1cb3d6846
Paulin <shemampa2010@gmail.com>
2026-06-25 10:21:36 +0200

    Generate the first N numbers in the Fibonacci sequence

--- dsa10.py
+++ dsa10.py
@@ -1,6 +1,6 @@
 nm = [0,1]
 where= int(input("Jusqu'a quel chiffre voulez vous arriver ?: "))
-if where == 0 or where == 1:
+if where < 1 :
     print ("A number over 1 pls")
 else:
     for i in range (0, where - 2):
